0a01c9ba679ba975545b473a91fc6824620be820
Jarek Kajko <jaruto7@gmail.com>
2019-08-18 15:44:43 +0200

    Add js to Increase number when user click Add glass button

--- src/js/main.js
+++ src/js/main.js
@@ -16,7 +16,31 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const glassCounter = document.querySelector('.glass__counter--js');
+const buttonAdd = document.querySelector('.glass__button-add--js');
+const buttonRemove = document.querySelector('.glass__button-remove--js');
 
-console.log(`Hello world!`);
+let counter = 0;
+const key = new Date().toISOString().slice(0, 10);
+
+
+
+buttonAdd.addEventListener('click', (e) => {
+  if(localStorage.getItem(key, counter)) {
+    counter++
+    glassCounter.innerHTML = counter;
+    localStorage.setItem(key, counter);
+  } else {
+    counter = 0;
+  }
+  
+  if(localStorage.getItem(key, counter) > 10) {
+    counter = 10;
+    glassCounter.innerHTML = counter;
+    localStorage.setItem(key, counter);
+  }
+});
+
+
 
 
